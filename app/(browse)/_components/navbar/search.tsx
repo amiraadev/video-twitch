4853d0fcf1f89fb7962b7b7d91f0631e96764640
amiraallagui <allagui_fawzi@outlook.fr>
2023-12-18 20:22:36 +0100

    onSubmit:this function essentially creates a URL that includes the term

--- app/(browse)/_components/navbar/search.tsx
+++ app/(browse)/_components/navbar/search.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import React from 'react'
+import React, { useState } from 'react'
 import qs from 'query-string'
 import {Search as SearchIcon ,X} from "lucide-react"
 import {useRouter} from "next/navigation"
@@ -12,10 +12,30 @@ import { Button } from '@/components/ui/button';
 
 
 export const Search = () => {
+   const router = useRouter();
+const [value, setValue] = useState("");
+ 
+/* this function essentially creates a URL that includes 
+ a query parameter term with a specific value derived from 
+ the value variable (if value is present and truthy), and then
+ redirects the application to that generated URL.*/
+const onSubmit = (e:React.FormEvent<HTMLFormElement>) =>{
+   e.preventDefault()
+   if(!value) return;
+   const url = qs.stringifyUrl({
+    url:"/",
+    query:{term:value}
+   },{skipEmptyString:true})
+
+   router.push(url)
+}
   return (
-    <form className='relative w-full lg:w-[400px] flex items-center'>
+    <form onSubmit={onSubmit} className='relative w-full lg:w-[400px] flex items-center'>
         <Input
+          value={value}
+          onChange={(e) => setValue(e.target.value)}
           placeholder='Search'
+          className='rounded-r-none focus-visible:ring-0 focus-visible:ring-transparent focus-visible:ring-offset-0'
           />
         <Button
         type='submit'

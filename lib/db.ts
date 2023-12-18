a71dfba347d70647ae90bc2225d9090b4775fb37
amiraallagui <allagui_fawzi@outlook.fr>
2023-12-18 11:07:36 +0100

    set up of the ngrok and documenting the instructions in the commands file

--- lib/db.ts
+++ lib/db.ts
@@ -1,8 +1,10 @@
+/** @format */
+
 import { PrismaClient } from "@prisma/client";
 declare global {
-    var prisma : PrismaClient |undefined
+	var prisma: PrismaClient | undefined;
 }
-export const db=globalThis.prisma || new PrismaClient()
-if(process.env.NODE_ENV !== 'production'){
-    globalThis.prisma = db
+export const db = globalThis.prisma || new PrismaClient();
+if (process.env.NODE_ENV !== "production") {
+	globalThis.prisma = db;
 }

a17140f5d888459f5679018cf7b24edced82a29b
amiraallagui <allagui_fawzi@outlook.fr>
2023-12-18 23:37:28 +0100

    fixing the collapse and the expand of the sidebar

---
+++ app/(browse)/_components/sidebar/wrapper.tsx
@@ -0,0 +1,21 @@
+"use client"
+import {cn} from '@/lib/utils';
+import { useSidebar } from "@/store/use-sidebar"
+
+interface WrapperProps {
+    children : React.ReactNode
+} 
+
+export const Wrapper = ({
+    children
+}:WrapperProps) => {
+    const {collapsed} =  useSidebar((state) => state)
+    return(
+        <aside className={cn(
+             "fixed left-0 flex flex-col w-60 h-full bg-background border-border-[#2D2E35] z-50",
+             collapsed && "w-[70px]"
+             )}>
+            {children}
+        </aside>
+    )
+}

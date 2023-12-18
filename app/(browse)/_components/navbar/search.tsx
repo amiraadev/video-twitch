cb3aa005239d1e2bc2c2c33a3124102ae081b896
amiraallagui <allagui_fawzi@outlook.fr>
2023-12-18 20:42:44 +0100

    getting done with the search component

--- app/(browse)/_components/navbar/search.tsx
+++ app/(browse)/_components/navbar/search.tsx
@@ -23,12 +23,16 @@ const onSubmit = (e:React.FormEvent<HTMLFormElement>) =>{
    e.preventDefault()
    if(!value) return;
    const url = qs.stringifyUrl({
-    url:"/",
+    url:"/search",
     query:{term:value}
    },{skipEmptyString:true})
 
    router.push(url)
 }
+
+const onClear = () => {
+    setValue("")
+};
   return (
     <form onSubmit={onSubmit} className='relative w-full lg:w-[400px] flex items-center'>
         <Input
@@ -37,6 +41,12 @@ const onSubmit = (e:React.FormEvent<HTMLFormElement>) =>{
           placeholder='Search'
           className='rounded-r-none focus-visible:ring-0 focus-visible:ring-transparent focus-visible:ring-offset-0'
           />
+        { value && (
+                <X 
+                className='absolute top-2.5 right-14 h-5 w-5 text-muted-foreground cursor-pointer hover:opacity-75 transition'
+                onClick={onClear}
+                />
+            ) }
         <Button
         type='submit'
         size='sm'

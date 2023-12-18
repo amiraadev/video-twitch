c1613a5b671fd78822ea5361ece4f55984d26b97
amiraallagui <allagui_fawzi@outlook.fr>
2023-12-18 16:55:07 +0100

    restructuring the page.tsx : put it under (browse)/(home) and redifine the layout.tsx

---
+++ app/(browse)/layout.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const BrowseLayout = ({children}:{children:React.ReactNode}) => {
+  return (
+    <div>
+      {children}
+    </div>
+  )
+}
+
+export default BrowseLayout

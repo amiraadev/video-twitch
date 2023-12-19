ac9c555fa21f140bf11788e7fc188713d64e0576
amiraallagui <allagui_fawzi@outlook.fr>
2023-12-19 14:39:29 +0100

    adding this : await new Promise(resolve => setTimeout(resolve,5000)) to recommended-service to fix the suspense bug

--- app/(browse)/_components/sidebar/index.tsx
+++ app/(browse)/_components/sidebar/index.tsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import { Wrapper } from './wrapper'
 import { Toggle } from './toggle'
-import { Recommended } from './recommended'
+import { Recommended, RecommendedSkeleton } from './recommended'
 import { getRecommended } from '@/lib/recommended-service'
 
 export const Sidebar = async () => {
@@ -14,5 +14,13 @@ export const Sidebar = async () => {
             <Recommended data={recommended}/>
         </div>
     </Wrapper>
+  )
+}
+
+export const SidebarSkeleton = () => {
+  return (
+    <aside  className='fixed left-0 flex flex-col w-[70px] lg:w-60 h-full bg-background border-r border-[#2D2E35] z-50'>
+      <RecommendedSkeleton />
+    </aside> 
   )
 }
--- lib/recommended-service.ts
+++ lib/recommended-service.ts
@@ -2,6 +2,7 @@ import { getSelf } from "./auth-service";
 import { db } from "./db";
 
 export const getRecommended = async () => {
+    await new Promise(resolve => setTimeout(resolve,5000))
     const users = await db.user.findMany({
         orderBy:{
             createdAt: "desc"

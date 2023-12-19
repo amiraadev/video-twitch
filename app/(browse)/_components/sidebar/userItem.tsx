156ed5ec587bc8266e8ab67eade8f3fe99eae9b3
amiraallagui <allagui_fawzi@outlook.fr>
2023-12-19 14:23:55 +0100

    UserItemSkeleton

--- app/(browse)/_components/sidebar/userItem.tsx
+++ app/(browse)/_components/sidebar/userItem.tsx
@@ -57,3 +57,14 @@ export const UserItem = ({ username, imageUrl, isLive }: UserItemProps) => {
 		</Button>
 	);
 };
+
+export const UserItemSkeleton = (  ) => {
+    return (
+        <li className="flex items-center gap-x-4 px-3 py-2">
+            <Skeleton className="min-h-[32px] min-w-[32px] rounded-full" />
+            <div className="flex-1">
+                <Skeleton className="h-6 "/>
+            </div>
+        </li>
+    )
+}

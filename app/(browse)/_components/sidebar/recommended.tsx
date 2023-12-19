221e4a1719df76a7e1708e0908fc517b1da192d5
amiraallagui <allagui_fawzi@outlook.fr>
2023-12-19 14:27:26 +0100

    UserItemSkeleton

--- app/(browse)/_components/sidebar/recommended.tsx
+++ app/(browse)/_components/sidebar/recommended.tsx
@@ -2,7 +2,7 @@
 import { useSidebar } from '@/store/use-sidebar'
 import { User } from '@prisma/client'
 import React from 'react'
-import { UserItem } from './userItem'
+import { UserItem, UserItemSkeleton } from './userItem'
 
 interface RecommendedProps {
     data:User[]
@@ -31,4 +31,16 @@ export const Recommended = ({data}:RecommendedProps) => {
         </ul>
     </div>
   )
+}
+
+export const RecommendedSkeleton = () => {
+    return (
+        <ul className='px-2'>
+            {
+                [...Array(3)].map((_,i) => (
+                    <UserItemSkeleton key={i} />
+                ))
+            }
+        </ul>
+    )
 }
